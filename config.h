c042163744531579fd1df1f0c6ba9a39244c2632
Klas Björkqvist <klas.bjorkqvist@gmail.com>
2013-01-27 19:53:48 +0100

    Remove name on tag 1

--- config.h
+++ config.h
@@ -18,7 +18,7 @@ static const Bool topbar            = True;     /* False means bottom bar */
 int struts[StrutLast] = { 15, 0, 15, 0 };
 
 /* tagging */
-static const char *tags[] = { "1:web", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
